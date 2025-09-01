dbf93bddee364a21cccd06662f14511e0cb41035
brandli <ch.braendli@gmail.com>
2025-09-01 22:43:20 +0200

    hide_enterprise_modules fixed

--- simple_task/__manifest__.py
+++ simple_task/__manifest__.py
@@ -3,6 +3,7 @@
     "version": "18.0.1.0.0",
     "summary": "A very simple Odoo module with a Task model.",
     "author": "Your Name",
+    "license": "LGPL-3",
     "category": "Productivity",
     "depends": ["base"],
     "data": [
